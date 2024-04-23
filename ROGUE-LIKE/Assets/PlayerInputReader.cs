2a6781e58919fe27755ed1e919d8b0cfe9e56807
SantoLeSaintPere <72297300+SantoLeSaintPere@users.noreply.github.com>
2024-04-23 15:46:41 +0200

    Set up for test

---
+++ ROGUE-LIKE/Assets/PlayerInputReader.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputReader : MonoBehaviour
+{
+    public PlayerInputControls inputcontrols;
+    public Vector3 direction;
+    public bool isMoving;
+    public bool isAttacking;
+    private void Awake()
+    {
+        inputcontrols = new PlayerInputControls();
+    }
+
+    private void OnEnable()
+    {
+        inputcontrols.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputcontrols.Player.Disable();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = inputcontrols.Player.MOVE.ReadValue<Vector2>();
+        direction = new Vector3(move.x,0, move.y);
+        direction.Normalize();
+        isMoving = direction.sqrMagnitude > 0;
+        isAttacking = inputcontrols.Player.ATTACK.IsPressed();
+
+        if(isAttacking )
+        {
+            Debug.Log("ATTACK");
+        }
+    }
+}
